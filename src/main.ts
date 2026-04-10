d320c8aa9b9cd0d42aef9b04ec45d3642fcdbbb5
ffillouxdev <fillouxflorian56@gmail.com>
2026-04-10 13:36:25 +0200

    refractor(): I come back to the Play CDN tailwindcss method

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="min-h-screen bg-background text-text p-8">
-    <h1 class="font-bold text-primary">Guide to Sugarcane Diseases</h1>
-  </div>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+  <main>
+    <h1 class="font-roboto font-thin">
+      Guide to Sugarcane Diseases
+    </h1>
+  </main>
 `
 
