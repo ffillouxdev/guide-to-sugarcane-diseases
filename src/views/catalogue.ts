0090a7672a057e918c6abbb105b250c1f862ac5d
ffillouxdev <fillouxflorian56@gmail.com>
2026-04-15 16:32:34 +0200

    style(): add main-bg.png as background image on all main elements

--- src/views/catalogue.ts
+++ src/views/catalogue.ts
@@ -4,7 +4,7 @@ import { callToAction } from '../components/call_to_action'
 export function catalogueView(): string {
   return /*html*/`
     ${header()}
-    <main class="max-w-5xl mx-auto px-6 py-10">
+    <main class="max-w-5xl mx-auto px-6 py-10 bg-[url('/assets/main-bg.png')] bg-cover bg-center">
       ${callToAction()}
     </main>
   `
