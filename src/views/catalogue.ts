7c232ad66c6a507539c54cba007ad2b905a8a6a0
ffillouxdev <fillouxflorian56@gmail.com>
2026-04-16 09:53:46 +0200

    style: set md:h-[93vh] on all main elements and use natural image dimensions in CTA

--- src/views/catalogue.ts
+++ src/views/catalogue.ts
@@ -4,7 +4,7 @@ import { callToAction } from '../components/call_to_action'
 export function catalogueView(): string {
   return /*html*/`
     ${header()}
-    <main class="max-w-5xl mx-auto px-6 py-10 bg-[url('/assets/main-bg.png')] bg-cover bg-center">
+    <main class="max-w-5xl mx-auto px-6 py-10 md:h-[93vh] bg-[url('/assets/main-bg.png')] bg-cover bg-center">
       ${callToAction()}
     </main>
   `
